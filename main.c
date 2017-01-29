4e9b2024a0e177e28af5997eb55f75ed1da08656
Simon Krix <simon@smonson.com>
2017-01-29 15:45:46 +1100

    Working rastering test pattern from RAM.

--- main.c
+++ main.c
@@ -39,6 +39,7 @@ volatile struct {
 
     // the ratio steps:total_steps matches x:pixels (where scanline[x] is PWM value)
     uint16_t pixels;
+    uint16_t scanline_index;
     
     uint16_t y_steps;
 } move_cmd;
@@ -59,27 +60,30 @@ ISR(TIMER1_COMPA_vect)
 	// X axis step pulse stop
 	PORTD &= ~_BV(PORTD2);
 	
+    if (move_cmd.reverse)
+    {
+        // Reverse: 1023 .. 0
+        if (move_cmd.steps-- == 0)
+        {
+            timer1_stop();
+            running = 0;
+	        return;
+        }
+    }
+    else
+    {
+        // Forward: 0 .. 1023
+        if (++move_cmd.steps == move_cmd.total_steps)
+        {
+            timer1_stop();
+            running = 0;
+	        return;
+        }
+    }
+        
+    // Move from acceleration lookup table:
     if (move_cmd.mode == MOVE_FROM_TABLE)
     {
-        if (move_cmd.reverse)
-        {
-	        if (move_cmd.steps-- == 0)
-	        {
-	            timer1_stop();
-	            running = 0;
-		        return;
-	        }
-        }
-        else
-        {
-	        if (++move_cmd.steps == move_cmd.total_steps)
-	        {
-	            timer1_stop();
-	            running = 0;
-		        return;
-	        }
-        }
-
         uint16_t new_duration = pgm_read_byte(LOOKUP + (move_cmd.steps * 2) + 1) << 8;
         new_duration |= pgm_read_byte(LOOKUP + (move_cmd.steps * 2));
         
@@ -89,17 +93,10 @@ ISR(TIMER1_COMPA_vect)
         return;
     }
     
-    // Not reading from acceleration lookup table:
-	if (++move_cmd.steps == move_cmd.total_steps)
-	{
-	    timer1_stop();
-	    running = 0;
-		return;
-	}
-
-    if (move_cmd.mode == MOVE_RASTER)
+    // Raster moves: control PWM value
+    else if (move_cmd.mode == MOVE_RASTER)
     {
-        // Adjust PWM
+        OCR2A = scanline[move_cmd.scanline_index + move_cmd.steps];  
     }
 }
 
@@ -108,7 +105,6 @@ ISR(TIMER1_COMPB_vect)
     // Begin X axis step pulse
 	PORTD |= _BV(PORTD2);
 }
-
 
 struct {
     int32_t xpos, ypos;
@@ -145,6 +141,7 @@ inline void setup()
 void flat_move(uint16_t rate, uint16_t steps)
 {
     move_cmd.mode = MOVE_NORMAL;
+    move_cmd.reverse = 0;
     move_cmd.steps = 0;
     move_cmd.total_steps = steps;
 	OCR1A = rate;
@@ -155,35 +152,54 @@ void flat_move(uint16_t rate, uint16_t steps)
 	{
 	}
 }
+
+void enable_laser_pwm()
+{
+    // Enable timer2 OC2A override on PORTB3 pin
+    TCCR2A |= _BV(COM2A1);
+}
+
+void disable_laser_pwm()
+{
+	timer2_stop();
+	
+	// Disable timer2 OC2A override on PORTB3 pin
+	TCCR2A &= ~(_BV(COM2A1) | _BV(COM2A0));
+	
+	// Ensure pin is driving low.
+	PORTB &= ~_BV(PORTB3);
+}
+
 /* A flat move WITH lasering */
-void raster_move(uint16_t rate, uint16_t steps)
+void raster_move(uint16_t rate, uint16_t steps, uint16_t index, uint8_t reverse)
 {
     move_cmd.mode = MOVE_RASTER;
-    move_cmd.steps = 0;
-    move_cmd.total_steps = steps;
+    move_cmd.reverse = reverse;
+    move_cmd.scanline_index = index;
 	OCR1A = rate;
     OCR1B = rate - 10;
+    move_cmd.total_steps = steps;
     
-    // setup for pwm
-    OCR2A = 255;
-    
+    // First pixel PWM value and step counter
+    if (reverse)
+    {
+        OCR2A = scanline[index + steps - 1];
+        move_cmd.steps = steps - 1;
+    }
+    else
+    {
+        OCR2A = scanline[index];
+        move_cmd.steps = 0;
+    }
     running = 1;
-
-	// enable timer2 OC2A pin
-	TCCR2A |= _BV(COM2A1);
-
-
+    enable_laser_pwm();
     timer1_start();
     timer2_start();
     
 	while(running)
 	{
 	}
-	
-	timer2_stop();
-	// disable timer2 OC2A pin
-	TCCR2A &= ~(_BV(COM2A1) | _BV(COM2A0));
-	PORTB &= ~_BV(PORTB3);
+	disable_laser_pwm();	
 }
 
 
@@ -238,6 +254,38 @@ void accel(uint16_t rate, uint8_t reverse, uint16_t pad_steps)
 	}
 }
 
+void y_advance(uint8_t steps)
+{
+    while (steps-- > 0)
+    {
+	    PORTD |= _BV(PORTD3);
+	    delay(1);
+        PORTD &= ~_BV(PORTD3);        	    
+	    delay(1);
+    }
+}
+
+void test_pattern()
+{
+    int i;
+    for (i = 0; i < 1024; i++)
+    {
+        if ((i % 256) < 4 || (i % 256) > 251)
+            scanline[i] = 255;
+        else
+            scanline[i] = i % 256;
+    }
+}
+
+void vertical_line()
+{
+    int i;
+    for (i = 0; i < 1024; i++)
+    {
+        scanline[i] = 255;
+    }
+}
+
 inline void test()
 {
     // Time to settle
@@ -254,50 +302,37 @@ inline void test()
     
     sei();
 
-	uint16_t velocity = 300;
-    uint16_t steps_per_line = 8;
-    uint16_t lines = 5;
+	uint16_t velocity = 2500;
+    uint16_t steps_per_line = 5;
+    uint16_t lines = 20;
     
     // Positive Y direction
     PORTD |= _BV(PORTD6);
     
 	while (lines-- > 0)
 	{
+	        if (lines < 2 || lines > 17)
+	            vertical_line();
+	        else
+	            test_pattern();
+	
 		    // Set direction to 1 (rightwards)
 		    PORTD |= _BV(PORTD5);
         	accel(velocity, 0, 1000); // speed up
-        	raster_move(velocity, 1024);
+        	raster_move(velocity, 1024, 0, 0);
         	accel(velocity, 1, 1000); // slow down
-#if 1
+
         	// step Y+
-        	uint8_t y_steps = steps_per_line;
-        	while (y_steps-- > 0)
-        	{
-        	    // FIXME: maybe too fast
-            	PORTD |= _BV(PORTD3);
-            	delay(1);
-                PORTD &= ~_BV(PORTD3);        	    
-            	delay(1);
-        	}
-#endif
+        	y_advance(steps_per_line);
+
 		    // Set direction to 0 (leftwards)
 		    PORTD &= ~_BV(PORTD5);
         	accel(velocity, 0, 1000); // speed up
-            raster_move(velocity, 1024);
+            raster_move(velocity, 1024, 0, 1);
         	accel(velocity, 1, 1000); // slow down
-#if 1
-        	// step Y+
-        	y_steps = steps_per_line;
-        	while (y_steps-- > 0)
-        	{
-        	    // FIXME: maybe too fast
-            	PORTD |= _BV(PORTD3);
-            	delay(1);
-                PORTD &= ~_BV(PORTD3);        	    
-            	delay(1);
-        	}
-#endif
 
+        	// step Y+
+        	y_advance(steps_per_line);
 	}
     cli();
 
